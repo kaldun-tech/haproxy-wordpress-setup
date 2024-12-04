82b19b8be27526ff2c7f980b9df692c09c34a2ad
Taras <tsmereka@protonmail.com>
2024-12-04 16:41:01 -0600

    working on the mysql setup

--- setup_mysql.sh
+++ setup_mysql.sh
@@ -1,14 +1,21 @@
 #/bin/bash
-# Setup MySQL
-# Requires a database name $NAME variable to be defined or as first argument
-if [ -z "$NAME" ]; then
-    if [ -z "$1" ]; then
-        echo "Please define a database name"
-        exit 1
-    else
-        NAME=$1
-    fi
-fi
+# Setup MySQL. Prompts for a root password
+# Define database name, user and password using the DB_NAME, DB_USER and DB_PASSWORD environment variables
+DB_NAME=${DB_NAME:-wordpress}
+DB_USER=${DB_USER}
+DB_PASSWORD=${DB_PASSWORD}
 
-NAME="sarcina" && sudo mysql --execute="CREATE DATABASE $NAME;CREATE USER $NAME@localhost;SET PASSWORD FOR $NAME@localhost = 'ColorAnimalNumber$';ALTER USER '$NAME'@'localhost' IDENTIFIED WITH mysql_native_password BY 'ColorAnimalNumber$';GRANT ALL PRIVILEGES ON $NAME.* TO $NAME@localhost;FLUSH PRIVILEGES;"
+# Prompt for root password
+read -s -p "Enter root password: " ROOT_PASSWORD
+
+# Install MySQL
+sudo apt-get install mysql-server mysql-client
+# Secure MySQL
+sudo mysql_secure_installation
+# Create Wordpress DB
+sudo mysql -u root -p${ROOT_PASSWORD} --execute="CREATE DATABASE $DB_NAME;"
+sudo mysql -u root -p${ROOT_PASSWORD} --execute="CREATE USER $DB_USER@localhost IDENTIFIED BY '$DB_PASSWORD';"
+sudo mysql -u root -p${ROOT_PASSWORD} --execute="GRANT ALL PRIVILEGES ON $DB_NAME.* TO $DB_USER@localhost;"
+sudo mysql -u root -p${ROOT_PASSWORD} --execute="FLUSH PRIVILEGES;"
+# Correct permissions for crontab cert renewal
 sudo chmod 007 /usr/local/sbin/le-renew-haproxy
--- var/www/$DOMAIN/public_html/wp-config.php
+++ var/www/$DOMAIN/public_html/wp-config.php
@@ -1,6 +1,7 @@
-# Need to add this to wp-config.php for SSL
-#
+# Enables force SSL
 define('FORCE_SSL_ADMIN', true);
 if (strpos($_SERVER['HTTP_X_FORWARDED_PROTO'], 'https') !== false)
        $_SERVER['HTTPS']='on';
-#
+# Set character set to utf8mb4 encoding for better Unicode support
+define('DB_CHARSET', 'utf8mb4');
+define('DB_COLLATE', 'utf8mb4_general_ci');
