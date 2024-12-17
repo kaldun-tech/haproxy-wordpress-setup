f32dbaee4c650f3f9768b18a1ecc5f66bd0ed53f
Taras <tsmereka@protonmail.com>
2024-12-16 20:45:30 -0600

    test lamp adjustments

--- scripts/tests/test_lamp.sh
+++ scripts/tests/test_lamp.sh
@@ -5,7 +5,7 @@ netstat -tlnp | grep 80
 if [ $? -eq 0 ]; then
     echo "Test 1: Apache is up and running"
 else
-    echo "Test 1: Apache is not up and running"
+    echo "Test 1: Apache is NOT up and running"
     exit 1
 fi
 
@@ -13,7 +13,7 @@ netstat -tlnp | grep 3306
 if [ $? -eq 0 ]; then
     echo "Test 2: MySQL is up and running"
 else
-    echo "Test 2: MySQL is not up and running"
+    echo "Test 2: MySQL is NOT up and running"
     exit 1
 fi
 
@@ -21,26 +21,24 @@ netstat -tlnp | grep 8080
 if [ $? -eq 0 ]; then
     echo "Test 4: Varnish is up and running"
 else
-    echo "Test 4: Varnish is not up and running"
+    echo "Test 4: Varnish is NOT up and running"
     exit 1
+fi
+
+php scripts/tests/test_php.php
+if [ $? -eq 0 ]; then
+  echo "Test 5: PHP installation test passed!"
+else
+  echo "Test 5: PHP installation test failed!"
+  exit 1
 fi
 
 ufw status
 if [ $? -eq 0 ]; then
-    echo "Test 5: Firewall is up and running"
+    echo "Test 6: Firewall is up and running"
 else
-    echo "Test 5: Firewall is not up and running"
+    echo "Test 6: Firewall is NOT up and running"
     exit 1
-fi
-
-# Test PHP installation
-echo "Testing PHP installation..."
-php scripts/tests/test_php.php
-if [ $? -eq 0 ]; then
-  echo "PHP installation test passed!"
-else
-  echo "PHP installation test failed!"
-  exit 1
 fi
 
 echo "All tests passed"
